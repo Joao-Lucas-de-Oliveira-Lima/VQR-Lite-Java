897b784785701561ac8d78d57fa63c42f4233c49
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2023-10-31 16:37:22 -0300

    adding ClienteModelImpl

---
+++ backEnd/src/main/java/com/vqr/backend/models/impl/ClientModelImpl.java
@@ -0,0 +1,28 @@
+package com.vqr.backend.models.impl;
+
+import com.vqr.backend.models.ClientModel;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.validator.internal.IgnoreForbiddenApisErrors;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "Clients")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class ClientModelImpl implements ClientModel, Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String cpf;
+}
