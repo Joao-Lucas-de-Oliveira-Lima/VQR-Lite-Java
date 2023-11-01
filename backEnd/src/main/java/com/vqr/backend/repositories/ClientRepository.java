cc70c9562e80c9cae281f1c08e8bd18f253ed052
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2023-11-01 05:02:51 -0300

    adding to ClienteRepository: search by name

--- backEnd/src/main/java/com/vqr/backend/repositories/ClientRepository.java
+++ backEnd/src/main/java/com/vqr/backend/repositories/ClientRepository.java
@@ -2,8 +2,14 @@ package com.vqr.backend.repositories;
 
 import com.vqr.backend.models.ClientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface ClientRepository extends JpaRepository<ClientModel, UUID>{
+public interface ClientRepository extends JpaRepository<ClientModel, UUID> {
+
+    List<ClientModel> findAllByName(String name);
+
 }
