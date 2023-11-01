7cb62bc934801c0e912bddeb720944402fbf87f4
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2023-11-01 05:05:35 -0300

    adding GET by name Endpoint for clients

--- backEnd/src/main/java/com/vqr/backend/controllers/ClientController.java
+++ backEnd/src/main/java/com/vqr/backend/controllers/ClientController.java
@@ -5,10 +5,11 @@ import com.vqr.backend.models.ClientModel;
 import com.vqr.backend.services.ClientService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ClientController {
@@ -21,7 +22,17 @@ public class ClientController {
 
     @PostMapping("/clients")
     public ResponseEntity<ClientModel> saveNewClient(@RequestBody @Valid ClientDto clientData){
-        return clientService.saveClient(clientData);
+        ClientModel result = clientService.saveClient(clientData);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
+    @GetMapping("/clients")
+    public ResponseEntity<List<ClientModel>> findAllClientsByName(
+            @RequestParam(value = "name") String name){
+        List<ClientModel> result = clientService.findAllClientsByName(name);
+        if(result.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.FOUND).body(result);
+    }
 }
